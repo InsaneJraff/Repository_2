4f8a12bde8374f2b5f37e9252b03c0324bd079e6
InsaneJraff <102656756+InsaneJraff@users.noreply.github.com>
2022-04-04 17:15:30 +0200

    dodano pokazywanie godziny

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import time
+
+
 def aa():
-    a = input("Wpisz swoje imie: ")
-    print(f'Siema: {a}')
+    a = input("Wpisz swoje imie: \n")
+    print(f'Siema {a}')
+
+def godzina():
+    t = time.localtime()
+    current_time = time.strftime('%H:%M:%S', t)
+    print('jest godzina ', current_time)
 
 aa()
+godzina()
